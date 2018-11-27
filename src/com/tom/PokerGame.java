465b2cd71aedd384744e20818ed09603a1aea768
Hank Tom <hanktom@gmail.com>
2018-11-27 10:25:58 +0800

    initial commit

---
+++ src/com/tom/PokerGame.java
@@ -0,0 +1,32 @@
+package com.tom;
+
+import java.util.Random;
+
+public class PokerGame {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int number = random.nextInt(13)+1;
+		int flower = random.nextInt(4);
+		System.out.print(number);
+		char c = 0;
+		switch(flower) {
+		case 0:
+			c = 'S';
+			break;
+		case 1:
+			c = 'H';
+			break;
+		case 2:
+			c = 'D';
+			break;
+		case 3:
+			c = 'C';
+			break;
+		}
+		System.out.println(c);
+		
+
+	}
+
+}
