be650a27f04565cefe1f2fdaf3940bdefa287b0c
user <user@es302nt>
2018-12-04 10:19:58 +0800

    a new poker deck

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -5,26 +5,31 @@ import java.util.Random;
 public class PokerGame {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-		int number = random.nextInt(13)+1;
-		int flower = random.nextInt(4);
-		System.out.print(number);
-		char c = 0;
-		switch(flower) {
-		case 0:
-			c = 'S';
-			break;
-		case 1:
-			c = 'H';
-			break;
-		case 2:
-			c = 'D';
-			break;
-		case 3:
-			c = 'C';
-			break;
-		}
-		System.out.println(c);
+		Poker poker = new Poker();
+		poker.print();
+		
+		
+		
+//		Random random = new Random();
+//		int number = random.nextInt(13)+1;
+//		int flower = random.nextInt(4);
+//		System.out.print(number);
+//		char c = 0;
+//		switch(flower) {
+//		case 0:
+//			c = 'S';
+//			break;
+//		case 1:
+//			c = 'H';
+//			break;
+//		case 2:
+//			c = 'D';
+//			break;
+//		case 3:
+//			c = 'C';
+//			break;
+//		}
+//		System.out.println(c);
 		
 
 	}
